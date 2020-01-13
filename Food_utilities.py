1c70b4adb0dad25254dffc7d3c31fb7b4c479b33
elw-arduino <minecraftisawsome007+github@gmail.com>
2020-01-13 14:10:21 -0500

    Add files via upload

---
+++ Food_utilities.py
@@ -0,0 +1,211 @@
+"""
+-------------------------------------------------------
+Food class utility functions.
+-------------------------------------------------------
+Author:  David Brown
+ID:      999999999
+Email:   dbrown@wlu.ca
+Section: CP164 Winter 2020
+__updated__ = "2020-01-13"
+-------------------------------------------------------
+"""
+from Food import Food
+
+
+def get_food():
+    """
+    -------------------------------------------------------
+    Creates a food object by requesting data from a user.
+    Use: f = get_food()
+    -------------------------------------------------------
+    Returns:
+        food - a completed food object (Food).
+    -------------------------------------------------------
+    """
+
+    # Your code here
+
+    return food
+
+
+def read_food(line):
+    """
+    -------------------------------------------------------
+    Creates and returns a food object from a line of string data.
+    Use: f = read_food(line)
+    -------------------------------------------------------
+    Parameters:
+        line - a vertical bar-delimited line of food data in the format
+          name|origin|is_vegetarian|calories (str)
+    Returns:
+        food - contains the data from line (Food)
+    -------------------------------------------------------
+    """
+
+    # Your code here
+
+    return food
+
+
+def read_foods(file_variable):
+    """
+    -------------------------------------------------------
+    Reads a file of food strings into a list of Food objects.
+    Use: foods = read_food(file_variable)
+    -------------------------------------------------------
+    Parameters:
+        file_variable - an open file of food data (file)
+    Returns:
+        foods - a list of food objects (list of Food)
+    -------------------------------------------------------
+    """
+
+    # Your code here
+
+    return foods
+
+
+def write_foods(file_variable, foods):
+    """
+    -------------------------------------------------------
+    Writes a list of food objects to a file.
+    file_variable contains the objects in foods as strings in the format
+          name|origin|is_vegetarian|calories
+    foods is unchanged.
+    Use: write_foods(file_variable, foods)
+    -------------------------------------------------------
+    Parameters:
+        file_variable - an open file of food data (file)
+        foods - a list of Food objects (list of Food)
+    Returns:
+        None
+    -------------------------------------------------------
+    """
+
+    # Your code here
+
+    return
+
+
+def get_vegetarian(foods):
+    """
+    -------------------------------------------------------
+    Creates a list of vegetarian foods.
+    foods is unchanged.
+    Use: v = get_vegetarian(foods)
+    -------------------------------------------------------
+    Parameters:
+        foods - a list of Food objects (list of Food)
+    Returns:
+        veggies - Food objects from foods that are vegetarian (list of Food)
+    -------------------------------------------------------
+    """
+
+    # Your code here
+
+    return veggies
+
+
+def by_origin(foods, origin):
+    """
+    -------------------------------------------------------
+    Creates a list of foods by origin.
+    foods is unchanged.
+    Use: v = by_origin(foods, origin)
+    -------------------------------------------------------
+    Parameters:
+        foods - a list of Food objects (list of Food)
+        origin - a food origin (int)
+    Returns:
+        origins - Food objects from foods that are of a particular origin (list of Food)
+    -------------------------------------------------------
+    """
+    assert origin in range(len(Food.ORIGIN))
+
+
+    # Your code here
+
+    return origins
+
+
+def average_calories(foods):
+    """
+    -------------------------------------------------------
+    Determines the average calories in a list of foods.
+    foods is unchanged.
+    Use: avg = average_calories(foods)
+    -------------------------------------------------------
+    Parameters:
+        foods - a list of Food objects (list of Food)
+    Returns:
+        avg - average calories in all Food objects of foods (int)
+    -------------------------------------------------------
+    """
+
+    # Your code here
+
+    return avg
+
+
+def calories_by_origin(foods, origin):
+    """
+    -------------------------------------------------------
+    Determines the average calories in a list of foods.
+    foods is unchanged.
+    Use: a = calories_by_origin(foods, origin)
+    -------------------------------------------------------
+    Parameters:
+        foods - a list of Food objects (list of Food)
+        origin - the origin of the Food objects to find (int)
+    Returns:
+        avg - average calories for all Foods of the requested origin (int)
+    -------------------------------------------------------
+    """
+    assert origin in range(len(Food.ORIGIN))
+
+    # Your code here
+
+    return avg
+
+
+def food_table(foods):
+    """
+    -------------------------------------------------------
+    Prints a formatted table of foods, sorted by name.
+    foods is unchanged.
+    Use: food_table(foods)
+    -------------------------------------------------------
+    Parameters:
+        foods - a list of Food objects (list of Food)
+    Returns:
+        None
+    -------------------------------------------------------
+    """
+
+    # Your code here
+
+    return
+
+
+def food_search(foods, origin, max_cals, is_veg):
+    """
+    -------------------------------------------------------
+    Searches for foods that fit certain conditions.
+    foods is unchanged.
+    Use: results = food_search(foods, origin, max_cals, is_veg)
+    -------------------------------------------------------
+    Parameters:
+        foods - a list of Food objects (list of Food)
+        origin - the origin of the food; if -1, accept any origin (int)
+        max_cals - the maximum calories for the food; if 0, accept any calories value (int)
+        is_veg - whether the food is vegetarian or not; if False accept any food (boolean)
+    Returns:
+        result - a list of foods that fit the conditions (list of Food)
+            foods parameter must be unchanged
+    -------------------------------------------------------
+    """
+    assert origin in range(-1, len(Food.ORIGIN))
+
+    # Your code here
+
+    return result
