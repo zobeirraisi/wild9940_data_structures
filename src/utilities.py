138aba33327043aa48b27b51cdd0ebcc753a1ede
elw-arduino <minecraftisawsome007+github@gmail.com>
2020-01-28 13:24:39 -0500

    Lab 3 uploaded

--- src/utilities.py
+++ src/utilities.py
@@ -1,15 +1,20 @@
 """
 -------------------------------------------------------
-Stack class Utility Functions
+Utility Functions
 -------------------------------------------------------
 Author:  Eric Wildfong
 ID:      190559940
 Email:   wild9940@mylaurier.ca
 Section: CP164 Winter 2020
-__updated__ = "2020-01-20"
+__updated__ = "2020-01-27"
 -------------------------------------------------------
 """
 from Stack_array import Stack
+from Queue_array import Queue
+from Priority_Queue_array import Priority_Queue
+
+#Stack
+#------------------------------------------
 
 def array_to_stack(stack, source):
     """
@@ -69,9 +74,152 @@ def stack_test(source):
         if not stack.is_empty():
             print("Stack is not empty")
             print("Peaking:",stack.peek())
-            print("Poping:",stack.pop())
+            print("Popping:",stack.pop())
         else:
             print('Stack is empty')
         stack.push(data)
         print('Pushed {} to stack'.format(data))
+    return
+
+#Queue
+#------------------------------------------
+
+def array_to_queue(queue, source):
+    """
+    -------------------------------------------------------
+    Inserts contents of source into queue. At finish, source is empty.
+    Last value in source is at rear of queue,
+    first value in source is at front of queue.
+    Use: array_to_queue(queue, source)
+    -------------------------------------------------------
+    Parameters:
+        queue - a Queue object (Queue)
+        source - a Python list (list)
+    Returns:
+        None
+    -------------------------------------------------------
+    """
+    while len(source) > 0:
+        queue.insert(source.pop(0))
+    return
+
+def queue_to_array(queue, target):
+    """
+    -------------------------------------------------------
+    Removes contents of queue into target. At finish, queue is empty.
+    Front value of queue is at front of target,
+    rear value of queue is at end of target.
+    Use: queue_to_array(queue, target)
+    -------------------------------------------------------
+    Parameters:
+        queue - a Queue object (Queue)
+        target - a Python list (list)
+    Returns:
+        None
+    -------------------------------------------------------
+    """
+    while not queue.is_empty():
+        target.append(queue.remove())
+    return
+
+def queue_test(a):
+    """
+    -------------------------------------------------------
+    Tests queue implementation.
+    Tests the methods of Queue are tested for both empty and
+    non-empty queues using the data in a:
+        is_empty, insert, remove, peek, len
+    Use: queue_test(a)
+    -------------------------------------------------------
+    Parameters:
+        a - list of data (list of ?)
+    Returns:
+        None
+    -------------------------------------------------------
+    """
+    q = Queue()
+
+    for data in a:
+        if not q.is_empty():
+            print("Queue is not empty")
+            print("Peaking:",q.peek())
+            print("Removing:",q.remove())
+        else:
+            print('Queue is empty')
+        q.insert(data)
+        print('Inserted {} to queue'.format(data))
+    return
+
+
+#Priority Queue
+#------------------------------------------
+
+def array_to_pq(pq, source):
+    """
+    -------------------------------------------------------
+    Inserts contents of source into pq. At finish, source is empty.
+    Last value in source is at rear of pq,
+    first value in source is at front of pq.
+    Use: array_to_pq(pq, source)
+    -------------------------------------------------------
+    Parameters:
+        pq - a Priority_Queue object (Priority_Queue)
+        source - a Python list (list)
+    Returns:
+        None
+    -------------------------------------------------------
+    """
+    while len(source) > 0:
+        pq.insert(source.pop(0))
+    return
+    
+
+def pq_to_array(pq, target):
+    """
+    -------------------------------------------------------
+    Removes contents of pq into target. At finish, pq is empty.
+    Highest priority value in pq is at front of target,
+    lowest priority value in pq is at end of target.
+    Use: pq_to_array(pq, target)
+    -------------------------------------------------------
+    Parameters:
+        pq - a Priority_Queue object (Priority_Queue)
+        target - a Python list (list)
+    Returns:
+        None
+    -------------------------------------------------------
+    """
+    while not pq.is_empty():
+        target.append(pq.remove())
+    return
+    
+
+def priority_queue_test(a):
+    """
+    -------------------------------------------------------
+    Tests priority queue implementation.
+    Test the methods of Priority_Queue are tested for both empty and
+    non-empty priority queues using the data in a:
+        is_empty, insert, remove, peek
+    Use: pq_test(a)
+    -------------------------------------------------------
+    Parameters:
+        a - list of data (list of ?)
+    Returns:
+        None
+    -------------------------------------------------------
+    """
+    pq = Priority_Queue()
+
+    for data in a:
+        if not pq.is_empty():
+            print("Priority Queue is not empty")
+            print("Peaking:",pq.peek())
+            print("Removing:",pq.remove())
+        else:
+            print('Priority Queue is empty')
+        pq.insert(data)
+        print('Inserted {} to priority queue'.format(data))
+    return
+
     return
