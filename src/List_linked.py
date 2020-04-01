7d53f024f5eef5a4f116058eb622661220fb1996
elw-arduino <minecraftisawsome007+github@gmail.com>
2020-03-31 22:14:32 -0400

    Lab 10

--- src/List_linked.py
+++ src/List_linked.py
@@ -610,7 +610,35 @@ class List:
             None
         -------------------------------------------------------
         """
-        # your code here
+        if pln is not prn:
+            # Swap only if two nodes are not the same node
+
+            if pln is None:
+                # Make r the new front
+                left = self._front
+                self._front = prn._next
+            else:
+                left = pln._next
+                pln._next = prn._next
+
+            if prn is None:
+                # Make l the new front
+                right = self._front
+                self._front = left
+            else:
+                right = prn._next
+                prn._next = left
+
+            # Swap next pointers
+            # lst._next, r._next = r._next, lst._next
+            temp = left._next
+            left._next = right._next
+            right._next = temp
+            # Update the rear
+            if right._next is None:
+                self._rear = right
+            elif left._next is None:
+                self._rear = left
         return
 
     def is_identical(self, target):
